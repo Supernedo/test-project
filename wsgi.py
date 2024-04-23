95d6ee1549728ed6d5d49be3a56bfad5267cfe2d
Ché Salazar <45917551+Supernedo@users.noreply.github.com>
2024-04-22 20:24:36 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@ from App.main import create_app
 from App.controllers import ( create_user, get_all_users_json, get_all_users )
 
 # Temp imports
-from App.models import *
+from models import *
 import requests
 import logging  # Import the logging module
 
